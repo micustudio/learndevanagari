9f5026c6a5e931f022cb3af804bd3b3fc87dbd46
Michael <Michael@michaels-mbp.attlocal.net>
2017-03-07 23:23:46 -0800

    switch domain to prod

--- src/app/app.domain.ts
+++ src/app/app.domain.ts
@@ -2,3 +2,7 @@ const local: string = 'http://localhost:3000/';
 const prod: string = 'https://intense-plains-43467.herokuapp.com/'
 
 export let domain: string = prod;
+
+// be sure that API routes do not match actual URL routes
+
+//-Michael
